74cdb8dc1588f8a163bc9969d5fddb4868e382ed
Daniel Chindea <chindea.daniel@gmail.com>
2013-11-20 19:31:21 +0200

    Update and rename calendar.js to parse-plugin.js

---
+++ www/parse-plugin.js
@@ -0,0 +1,11 @@
+var parsePlugin = {
+	getInstallationId: function(successCallback, errorCallback) {
+		cordova.exec(
+			successCallback,
+			errorCallback,
+			'org.apache.cordova.core.ParsePlugin',
+			'getInstallationId',
+			[]
+		);
+	}
+}
