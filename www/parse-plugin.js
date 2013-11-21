1288f87ee3232d9c3da54170ab5a77137bda7397
danielchindea <chindea.daniel@gmail.com>
2013-11-21 16:27:45 +0200

    as

--- www/parse-plugin.js
+++ www/parse-plugin.js
@@ -1,11 +1,9 @@
-var parsePlugin = {
-	getInstallationId: function(successCallback, errorCallback) {
-		cordova.exec(
-			successCallback,
-			errorCallback,
-			'org.apache.cordova.core.ParsePlugin',
-			'getInstallationId',
-			[]
-		);
-	}
+getInstallationId = function (successCallback, errorCallback) {
+    cordova.exec(
+        successCallback,
+        errorCallback,
+        'org.apache.cordova.core.ParsePlugin',
+        'getInstallationId',
+        []
+    );
 }
